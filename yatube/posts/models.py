4279886c8c8b2b364ae78b82cdc6fde685548f12
Федор Кулабухов <faust.kulabuhov@yandex.ru>
2023-02-10 21:31:49 +0300

    Второй

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -27,9 +27,8 @@ class Post(models.Model):
     class Meta:
         ordering = ('-pub_date',)
 
-
     def __str__(self):
-       return self.text[:15] 
+        return self.text[:15]
 
 
 class Group(models.Model):
